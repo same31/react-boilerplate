4e9e820b7d454c5cf1d375e96ec0dfc3963effc2
same31 <matthieu.mitrani@gmail.com>
2017-06-29 19:45:05 +0200

    + removed unnecessary code + package update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import CodeFest             from "./components/CodeFest/CodeFest"
 
 import './App.css';
 
@@ -18,7 +17,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <CodeFest />
+        It works!
       </div>
     );
   }
